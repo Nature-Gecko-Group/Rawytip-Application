f66e471a467e87ecabae47209da767661793d873
Nature Gecko <59920578+NatureGecko@users.noreply.github.com>
2023-12-28 22:14:42 +0700

    Update 0 : Update Tailwind CSS
    Not yet done.

--- rawytip-client/nuxt.config.ts
+++ rawytip-client/nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  ssr: false,
+  css: ['~/assets/css/index.scss'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
---
+++ rawytip-client/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-black w-full h-screen flex items-center justify-center">
+    <div class="bg-white p-4 w-1/3">
+      <div class="text-red-900 font-kanit text-xl">
+        โดยที่ประชาชนแห่งสหประชาชาติได้ยืนยันอีกครั้งไว้ในกฎบัตรถึงศรัทธาในสิทธิมนุษยชนขั้นพื้นฐาน
+      </div>
+    </div>
+  </div>
+</template>
